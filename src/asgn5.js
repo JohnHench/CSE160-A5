57a354ce9a1c603b421d37ac98955b4ec5e18dd3
Johnny Hench <johnnyplays1@gmail.com>
2024-05-13 19:24:54 -0700

    some updates

--- src/asgn5.js
+++ src/asgn5.js
@@ -17,6 +17,20 @@ async function main() {
     
     initializeLight(scene);
 
+    function skyboxInit(scene) { 
+        const loader = new THREE.CubeTextureLoader();
+        const texture = loader.load([
+        '../background/right.png',  // Positive X (right)
+        '../background/left.png',   // Negative X (left)
+        '../background/top.png',   // Positive Y (top)
+        '../background/bottom.png',  // Negative Y (bottom)
+        '../background/front.png',  //  Positive Z (front)
+        '../background/back.png',    //  Negative Z (back)
+        ]);
+        scene.background = texture;
+    }
+
+    skyboxInit(scene);
     // ----- FLOOR SETUP -----
     setupFloor(scene);
 
@@ -92,6 +106,22 @@ function initializeLight(scene) {
     aboveTreeLight.target.position.set(0, 0, 0);
     scene.add(aboveTreeLight);
     scene.add(aboveTreeLight.target);
+
+    // Create a HemisphereLight
+    const hemiLight = new THREE.HemisphereLight(color, intensity);
+    hemiLight.position.set(0, 10, 0);
+    scene.add(hemiLight);
+
+    const pointLight = new THREE.PointLight(0xebd234, 1); // color, intensity
+    pointLight.position.set(-12, 5.5, 0); // Set position of the light
+    scene.add(pointLight); // Add the light to the scene
+    pointLight.shadow.mapSize.width = 1024; // Width of the shadow map
+    pointLight.shadow.mapSize.height = 1024; // Height of the shadow map
+    pointLight.shadow.camera.near = 0.5; // Near shadow camera distance
+    pointLight.shadow.camera.far = 50; // Far shadow camera distance
+
+    const pointLightHelper = new THREE.PointLightHelper(pointLight, 1); // Light, size of the helper
+    scene.add(pointLightHelper); // Add helper to the scene
 }
 
 function setupFloor(scene) {
@@ -150,7 +180,38 @@ async function loadModels(scene) {
     } catch (error) {
         console.error('Error loading model:', error);
     }
+
+
+    try {
+        const mtl = await new Promise((resolve, reject) => {
+            materialLoader.load('../models/Lamp/rv_lamp_post_3.mtl', resolve, undefined, reject);
+        });
+        mtl.preload();
+
+        // for (const material of Object.values(mtl.materials)) {
+        //     material.side = THREE.DoubleSide;
+        //     // Apply texture here
+        //     const textureLoader = new THREE.TextureLoader();
+        //     const texture = textureLoader.load('../Textures/abstract-alien-metal_albedo.png');
+        //     texture.encoding = THREE.sRGBEncoding;
+        //     material.map = texture;
+        // }
+
+        objLoader.setMaterials(mtl);
+
+        const root = await new Promise((resolve, reject) => {
+            objLoader.load('../models/Lamp/rv_lamp_post_3.obj', resolve, undefined, reject);
+        });
+
+        root.scale.set(0.3, 0.3, 0.3);
+        root.rotation.y = 135;
+        root.position.set(-12, 0.05, 0);
+        scene.add(root);
+    } catch (error) {
+        console.error('Error loading model:', error);
+    }
 }
+
 
 function createCubes(scene) {
     const cubes = [];
@@ -159,16 +220,16 @@ function createCubes(scene) {
     const cubeDepth = 6;
 
     // LOG 1
-    createCube(scene, 1, -.9, 0);
+    createLog(scene, 1, -.9, 0);
 
     // LOG 2
-    createCube(scene, -1, -.9, 0);
+    createLog(scene, -1, -.9, 0);
 
     // LOG 3
-    createCube(scene, 0, -.9, 1, Math.PI / 2);
+    createLog(scene, 0, -.9, 1, Math.PI / 2);
 
     // LOG 4
-    createCube(scene, 0, -.9, -1, Math.PI / 2);
+    createLog(scene, 0, -.9, -1, Math.PI / 2);
 
     // FIRE MODEL
     const geometry = new THREE.IcosahedronGeometry(1);
@@ -178,10 +239,52 @@ function createCubes(scene) {
     cubes.push(mesh);
     scene.add(mesh);
 
+    // Path to fire
+    createCube(scene, .6, -.4, 5, 1, "gray");
+    createCube(scene, -0.6, -.4, 6, 1, "gray");
+    createCube(scene, 0.6, -.4, 7, 1, "gray");
+    
+    createCube(scene, -0.6, -.4, 8, 1, "gray");
+    createCube(scene, 0.6, -.4, 9, 1, "gray");
+    createCube(scene, -0.6, -.4, 10, 1, "gray");
+
+    createCube(scene, 0.6, -.4, 11, 1, "gray");
+    createCube(scene, -0.6, -.4, 12, 1, "gray");
+    createCube(scene, 0.6, -.4, 13, 1, "gray");
+
+
+    // Wall Front and Back
+    createCube(scene, -9, .5, 10, 2, "brown");
+    createCube(scene, -9, .5, 10, 2, "brown");
+    createCube(scene, 9, .5, 10, 2, "brown");
+    createCube(scene, -9, .5, -10, 2, "brown");
+    createCube(scene, 9, .5, -10, 2, "brown");
+
+    // Wall Left and Right
+    createCube(scene, 15.25, .5, 0, 3, "brown");
+    createCube(scene, -15.25, .5, 0, 3, "brown");
+
+    // Wall Posts Front
+    createCube(scene, 2, .5, 10, 4, "brown");
+    createCube(scene, -2, .5, 10, 4, "brown");
+
+    // Wall Posts Back
+    createCube(scene, 2, .5, -10, 4, "brown");
+    createCube(scene, -2, .5, -10, 4, "brown");
+
+    // Grave Head Stone Cylinder Part
+    createCube(scene, -13, 1.75, -8, 5, "gray", Math.PI / 2, 0, 0);
+    // Grave Head Stone Cube Part
+    createCube(scene, -13, 1, -8, 6, "gray");
+    // Grave
+    createCube(scene, -13, -.9, -6, 7, "brown");
+
+
+
     return cubes;
 }
 
-function createCube(scene, x, y, z, rotationZ = 0) {
+function createLog(scene, x, y, z, rotationZ = 0) {
     const geometry = new THREE.CylinderGeometry(1, 1, 3, 20);
     const material = new THREE.MeshPhongMaterial({ color: new THREE.Color(101/255, 67/255, 33/255) });
     const mesh = new THREE.Mesh(geometry, material);
@@ -191,6 +294,75 @@ function createCube(scene, x, y, z, rotationZ = 0) {
     mesh.rotation.z = rotationZ;
     mesh.position.set(x, y, z);
     scene.add(mesh);
+}
+
+function createCube(scene, x, y, z, size, color, rotationX = 0, rotationY = 0,  rotationZ = 0) {
+    const geometry = createGeometry(size);
+    const material = createMaterial(color);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.rotation.x = rotationX;
+    mesh.rotation.y = rotationY;
+    mesh.rotation.z = rotationZ;
+    mesh.position.set(x, y, z);
+    scene.add(mesh);
+}
+
+function createGeometry(size) {
+    let geometry;
+    switch (size) {
+        // Cube for path
+        case 1:
+            geometry = new THREE.BoxGeometry(1, 1, 1);
+            break;
+        // Wall Front and back
+        case 2:
+            geometry = new THREE.BoxGeometry(13, 3, .5); 
+            break;
+        // Wall Left and Right
+        case 3:
+            geometry = new THREE.BoxGeometry(.5, 3, 20); 
+            break;
+        // Wall Posts 
+        case 4:
+            geometry = new THREE.BoxGeometry(1, 5, .8); 
+            break;
+        // Grave Head Stone Cylinder Part
+        case 5:
+            geometry = new THREE.CylinderGeometry(.75, .75, .25, 20);
+            break;
+        // Grave Head Stone Cube Part
+        case 6:
+            geometry = new THREE.BoxGeometry(1.5, 2.0, .255);
+            break; 
+        // Grave     
+        case 7:
+            geometry  = new THREE.BoxGeometry(2, 2.0, 3);
+            break;
+        // Add more cases for different sizes if needed
+        default:
+            geometry = new THREE.BoxGeometry(size, size, size);
+            break;
+    }
+    return geometry;
+}
+
+function createMaterial(color) {
+    let material;
+    switch (color) {
+        case "gray":
+            material = new THREE.MeshPhongMaterial({ color: new THREE.Color(100/255, 107/255, 104/255) });
+            break;
+        case "brown":
+            material = new THREE.MeshPhongMaterial({ color: new THREE.Color(10/255, 5/255, 0/255) });
+            break;
+        // Add more cases for different colors if needed
+        default:
+            material = new THREE.MeshPhongMaterial({ color: new THREE.Color(1, 1, 1) }); // Default white color
+            break;
+    }
+    return material;
 }
 
 function animateCubes(time, cubes) {
@@ -203,3 +375,5 @@ function animateCubes(time, cubes) {
 }
 
 main();
+
+
